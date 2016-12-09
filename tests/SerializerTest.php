b7e8f352a83c7b0994bd39c00687025fde5170f4
Craig Duncan <craig@metroplexsystems.co.uk>
2016-12-09 09:45:02 +0000

    Rename the tests to show what they are testing

--- tests/SerializerTest.php
+++ tests/SerializerTest.php
@@ -23,47 +23,65 @@ class SerializerTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($expected, $message);
     }
 
-    /**
-     * @dataProvider segments
-     */
-    public function testSerializer($expected, $segments)
+
+    public function testBasic1()
     {
-        $this->assertSegments($expected, $segments);
+        $this->assertSegments("RFF+PD:50515", [
+            new Segment("RFF", ["PD", "50515"]),
+        ]);
+    }
+    public function testBasic2()
+    {
+        $this->assertSegments("RFF+PD+50515", [
+            new Segment("RFF", "PD", "50515"),
+        ]);
     }
 
-    public function segments()
+
+    public function testEscapeCharacter()
     {
-        return [
-            [
-                "DTM+735:?+0000:406",
-                [
-                    new Segment("DTM", ["735", "+0000", "406"])
-                ]
-            ],
-            [
-                "QTY+136:12,235",
-                [
-                    new Segment("QTY", ["136", "12,235"]),
-                ]
-            ],
-            [
-                "ERC+10:The message does not make sense??",
-                [
-                    new Segment("ERC", ["10", "The message does not make sense?"]),
-                ]
-            ],
-            [
-                "RFF+PD:50515",
-                [
-                    new Segment("RFF", ["PD", "50515"])
-                ]
-            ],
-            [
-                "RFF+PD+50515",
-                [
-                    new Segment("RFF", "PD", "50515"),
-                ]
-            ]
-        ];
+        $this->assertSegments("ERC+10:The message does not make sense??", [
+            new Segment("ERC", ["10", "The message does not make sense?"]),
+        ]);
+    }
+
+
+    public function testEscapeComponentSeparator()
+    {
+        $this->assertSegments("ERC+10:Name?: Craig", [
+            new Segment("ERC", ["10", "Name: Craig"]),
+        ]);
+    }
+
+
+    public function testEscapeDataSeparator()
+    {
+        $this->assertSegments("DTM+735:?+0000:406", [
+            new Segment("DTM", ["735", "+0000", "406"]),
+        ]);
+    }
+
+
+    public function testEscapeDecimalPoint()
+    {
+        $this->assertSegments("QTY+136:12,235", [
+            new Segment("QTY", ["136", "12,235"]),
+        ]);
+    }
+
+
+    public function testEscapeSegmentTerminator()
+    {
+        $this->assertSegments("ERC+10:Craig?'s", [
+            new Segment("ERC", ["10", "Craig's"]),
+        ]);
+    }
+
+
+    public function testEscapeSequence()
+    {
+        $this->assertSegments("ERC+10:?:?+???' - ?:?+???' - ?:?+???'", [
+            new Segment("ERC", ["10", ":+?' - :+?' - :+?'"]),
+        ]);
     }
 }

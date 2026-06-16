djandyr/edifact-2

Add support for Tradacoms messages
Tradacoms use = to represent the end of the segment name (rather than the data separator).
So this change allows a different tag to be used, but ensures the data separator is the default

src/Exceptions/BadMethodCallException.php
@@ -1,0 +1,7 @@
+<?php
+
+namespace Metroplex\Edifact\Exceptions;
+
+class BadMethodCallException extends \BadMethodCallException implements Exception
+{
+}

tests/InAndOutTest.php
@@ -2,8 +2,13 @@
 
 namespace Metroplex\EdifactTests;
 
+use Metroplex\Edifact\Control\Characters;
+use Metroplex\Edifact\Control\Tradacoms;
 use Metroplex\Edifact\Message;
+use Metroplex\Edifact\Parser;
+use Metroplex\Edifact\Serializer;
 use function file_get_contents;
+use function iterator_to_array;
 use function str_replace;
 
 class CompleteTest extends \PHPUnit_Framework_TestCase
@@ -23,6 +28,22 @@ class CompleteTest extends \PHPUnit_Framework_TestCase
         $output = (string) Message::fromFile($file);
 
         $message = file_get_contents($file);
+        $expected = str_replace("\n", "", $message);
+
+        $this->assertSame($expected, $output);
+    }
+
+
+    public function testTradacoms()
+    {
+        $message = file_get_contents(__DIR__ . "/data/tradacoms.edi");
+
+        $characters = new Tradacoms;
+        $segments = (new Parser)->parse($message, $characters);
+
+        $output = (new Serializer($characters))->serialize(...$segments);
+        $output = preg_replace("/^UNA.{5}'/", "", $output);
+
         $expected = str_replace("\n", "", $message);
 
         $this->assertSame($expected, $output);

estratocloud/edifact

src/Exceptions/BadMethodCallException.php
@@ -1,0 +1,7 @@
+<?php
+
+namespace Estrato\Edifact\Exceptions;
+
+class BadMethodCallException extends \BadMethodCallException implements Exception
+{
+}

tests/InAndOutTest.php
@@ -2,7 +2,10 @@
 
 namespace Estrato\EdifactTests;
 
+use Estrato\Edifact\Control\Tradacoms;
 use Estrato\Edifact\Message;
+use Estrato\Edifact\Parser;
+use Estrato\Edifact\Serializer;
 use PHPUnit\Framework\TestCase;
 
 use function file_get_contents;
@@ -30,6 +33,22 @@ class InAndOutTest extends TestCase
         $output = (string) Message::fromFile($file);
 
         $message = (string) file_get_contents($file);
+        $expected = str_replace("\n", "", $message);
+
+        $this->assertSame($expected, $output);
+    }
+
+
+    public function testTradacoms1(): void
+    {
+        $message = (string) file_get_contents(__DIR__ . "/data/tradacoms.edi");
+
+        $characters = new Tradacoms();
+        $segments = (new Parser())->parse($message, $characters);
+
+        $output = (new Serializer($characters))->serialize(...$segments);
+        $output = preg_replace("/^UNA.{5}'/", "", $output);
+
         $expected = str_replace("\n", "", $message);
 
         $this->assertSame($expected, $output);